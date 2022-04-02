358789c6deda9112ff97721267df4b8c65fd3122
Teerapath Sujitto <teerapart.s@gmail.com>
2022-04-03 02:56:26 +0700

    do page

--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Main } from './pages/main';
-const container = document.getElementById('root');
-ReactDOM.render(<Main/>, container);
